04c7b32f81dc9509baab6e208d799c77e620569a
rajunahidhasan0 <mahadyhasankbc710@gmail.com>
2018-05-01 11:20:32 +0600

    Add a funtion for summing to intiger

--- Assignment.cpp
+++ Assignment.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+void sum(int a ,int b){
+	cout<<"Enter Two Intiger :";
+	cin>>a>>b;
+	cout<<"Sum = "<<a+b<endl;
+}
+
 int main()
 {
 	int a,b,c;
+	sum();
 }
